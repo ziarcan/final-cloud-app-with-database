80168b7aa4fe5775d574daaa04273cdae9a91a41
Branislav Balaz <branislav.balaz@xit.camp>
2022-08-05 10:57:01 +0200

    In Progress exam results (fixed is get score)

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -113,7 +113,8 @@ class Question(models.Model):
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
+        wrong_answers = self.choice_set.filter(is_correct=False, id__in=selected_ids).count()
+        if all_answers == selected_correct and wrong_answers == 0:
             return True
         else:
             return False
